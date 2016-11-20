12f14deea226f3fd65483601a242e6d7eebb1917
GabrielePrestifilippo <gabry.501@live.it>
2016-11-20 10:40:17 +0100

    test1

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var restify = require('restify');
 var builder = require('botbuilder');
 var intents = new builder.IntentDialog();
 var https = require('https');
+var httpreq = require('httpreq');
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
@@ -93,7 +94,7 @@ dialog.matches('search train', [
 
                 session.dialogData.timestamp = session.dialogData.timestamp.toISOString();
             }
-            var myRequest = '/v1/connections?from=' +
+            var myRequest = 'http://transport.opendata.ch' + '/v1/connections?from=' +
                 session.dialogData.source +
                 '&to=' + session.dialogData.destination +
                 '&datetime=' +
@@ -105,77 +106,79 @@ dialog.matches('search train', [
             session.sendTyping();
 
 
-            console.log("transport.opendata.ch" + myRequest);
-
-            var options = {
-                host: 'transport.opendata.ch',
-                path: myRequest,
-                method: 'GET',
-                headers: {
-                    accept: '*/*'
-                }
-            };
-            var bodyReq = [];
-            var req = https.request(options, function (res) {
-                console.log(res.statusCode);
-                res.on('data', function (d) {
-                    bodyReq.push(d);
-                    console.log("data");
-                });
-
-                res.on('end', function (d) {
-                    console.log(end);
-                    bodyReq = Buffer.concat(bodyReq).toString();
-
-                    if (bodyReq.error) {
-                        session.send("No solutions found");
-                        session.endDialog();
-                    }
-                    session.send("There are " + bodyReq.connections.length + " solutions");
-                    session.dialogData.tickets = bodyReq.connections;
-                    for (var x = 0; x < bodyReq.connections.length; x++) {
-
-                        var mySolutions = printSolution(bodyReq.connections[x]);
-                        session.send((x + 1) + ") " + mySolutions);
-                    }
-                });
-                req.end();
-
-                req.on('error', function (e) {
-                    session.send("error");
-                    body = Buffer.concat(body).toString();
-                    console.log(e);
-                });
-            });
+            console.log(myRequest);
             /*
-             try {
-             httpreq.get(myRequest, function (err, res) {
-             if (err) {
-             return console.log(err);
-             session.send(err);
+             var options = {
+             host: 'transport.opendata.ch',
+             path: myRequest,
+             method: 'GET',
+             headers: {
+             accept: '*
+             *
+             '
              }
-             var mybody = JSON.parse(res.body);
-             try {
-             session.send("There are " + mybody.connections.length + " solutions");
-             session.dialogData.tickets = mybody.connections;
-             for (var x = 0; x < mybody.connections.length; x++) {
-
-             var mySolutions = printSolution(mybody.connections[x]);
-             session.send((x + 1) + ") " + mySolutions);
-
-             }
-             console.log("now next");
-             next();
-             } catch (e) {
-             session.send(JSON.stringify(e));
-             }
-
+             };
+             var bodyReq = [];
+             var req = https.request(options, function (res) {
+             console.log(res.statusCode);
+             res.on('data', function (d) {
+             bodyReq.push(d);
+             console.log("data");
              });
+
+             res.on('end', function (d) {
+             console.log("end");
+             bodyReq = Buffer.concat(bodyReq).toString();
+
+             if (bodyReq.error) {
+             session.send("No solutions found");
+             session.endDialog();
              }
-             catch (e) {
-             session.send(JSON.stringify(e));
+             session.send("There are " + bodyReq.connections.length + " solutions");
+             session.dialogData.tickets = bodyReq.connections;
+             for (var x = 0; x < bodyReq.connections.length; x++) {
+
+             var mySolutions = printSolution(bodyReq.connections[x]);
+             session.send((x + 1) + ") " + mySolutions);
              }
+             });
+             req.end();
+
+             req.on('error', function (e) {
+             session.send("error");
+             body = Buffer.concat(body).toString();
+             console.log(e);
+             });
+             });
              */
+            try {
+                httpreq.get(myRequest, function (err, res) {
+                    if (err) {
+                        return console.log(err);
+                        session.send(err);
+                    }
+                    var mybody = JSON.parse(res.body);
+                    try {
+                        session.send("There are " + mybody.connections.length + " solutions");
+                        session.dialogData.tickets = mybody.connections;
+                        for (var x = 0; x < mybody.connections.length; x++) {
+
+                            var mySolutions = printSolution(mybody.connections[x]);
+                            session.send((x + 1) + ") " + mySolutions);
+
+                        }
+                        console.log("now next");
+                        next();
+                    } catch (e) {
+                        session.send(JSON.stringify(e));
+                    }
+
+                });
+            }
+            catch (e) {
+                session.send(JSON.stringify(e));
+            }
+
 
         }
         else {
@@ -217,7 +220,8 @@ dialog.matches('search train', [
     }
 
 
-]);
+])
+;
 
 
 function printProductSolution(connection) {
